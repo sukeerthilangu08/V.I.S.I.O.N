b901f6143b8ed8fbe0f6e0645d200d755ec70bc5
Sukeerthi <sukeerthi.langu@gmail.com>
2025-08-27 21:15:11 +0530

    feat: Add speech recognition

--- vision.py
+++ vision.py
@@ -7,6 +7,19 @@ import datetime
 import random
 import numpy as np
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from V.I.S.I.O.N"
+
 if __name__ == '__main__':
     print('Welcome to V.I.S.I.O.N')
     # Main loop will go here
