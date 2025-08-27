6fe57577f2255469ee8277dcca3a02d3d3ee4b4f
Sukeerthi <sukeerthi.langu@gmail.com>
2025-08-27 21:14:45 +0530

    feat: Add basic project structure

---
+++ vision.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+from config import apikey
+import datetime
+import random
+import numpy as np
+
+if __name__ == '__main__':
+    print('Welcome to V.I.S.I.O.N')
+    # Main loop will go here
