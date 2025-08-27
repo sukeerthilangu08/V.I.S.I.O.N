3e85c054ac9d0eceb8de9bf7cd298eea0987804b
Sukeerthi <sukeerthi.langu@gmail.com>
2025-08-27 21:16:30 +0530

    feat: Implement core chat functionality

--- vision.py
+++ vision.py
@@ -7,6 +7,25 @@ import datetime
 import random
 import numpy as np
 
+chatStr = ""
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = apikey
+    chatStr += f"User: {query}\n V.I.S.I.O.N: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
 def say(text):
     os.system(f'say "{text}"')
 
